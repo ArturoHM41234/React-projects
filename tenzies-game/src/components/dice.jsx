c1565a57584c649197bec14024d08494190b2c27
ArturoHM41234 <arturoh2672@gmail.com>
2023-01-02 14:19:42 -0600

    add dots to the dices instead of numbers

--- tenzies-game/src/components/dice.jsx
+++ tenzies-game/src/components/dice.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { nanoid } from "nanoid";
 
 export default function Dice(props) {
     const styles = {
 	backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF"
-    }
+    };
+
+	const Dots = () => {
+		const dots = [];
+		for (let i = 0; i < props.number; i++) {
+			dots.push(<span key={ nanoid() } className="dot"></span>);
+		}
+		return (<div className="dots--container">{dots}</div>)
+	}
+
+
     return (
 	<div className="dice" style={ styles }
 	     onClick={ () => {props.holdDice(props.id)} }>
-	    <p>{props.number}</p>
+		<Dots />
 	</div>
     );
 }
